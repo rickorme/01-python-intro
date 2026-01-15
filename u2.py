d242ca2bdd52bfe8f5df481d54851c9d61519431
Rick Orme <rick.orme@gmail.com>
2026-01-15 21:05:33 +0100

    uppgiffterna 2 och 3

---
+++ u2.py
@@ -0,0 +1,6 @@
+pris = 100  # biljettpris
+pengar = 200  # pengar på fickan
+pengar = pengar - pris
+print("Det blir " + str(pengar) + " kronor över.")
+share = pengar / 2
+print("Varje person får " + str(share))
---
+++ u3.1.py
@@ -0,0 +1,10 @@
+my_int1 = input("Please enter an integer: ")
+my_int1 = int(my_int1)
+print("You entered: " + str(my_int1))
+
+my_int2 = input("Please enter another integer: ")
+my_int2 = int(my_int2)
+print("You entered: " + str(my_int2))
+
+total = my_int1 + my_int2
+print("The sum is: " + str(total))
